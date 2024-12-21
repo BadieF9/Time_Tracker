29be7032661c5d97a1915ae9ea806910ecf4b63b
Badie <badie.farzandi@gmail.com>
2024-12-21 13:18:13 +0330

    Added required things to TimeEntryProvider

--- lib/providers/time_entry_provider.dart
+++ lib/providers/time_entry_provider.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:localstorage/localstorage.dart';
 import 'package:time_tracker/models/time_entry.dart';
 
 class TimeEntryProvider with ChangeNotifier {
+  final LocalStorage storage;
   List<TimeEntry> _entries = [];
   List<TimeEntry> get entries => _entries;
+
+  TimeEntryProvider({required this.storage}) {
+    _loadTimeEntriesFromStorage();
+  }
+
+  void _loadTimeEntriesFromStorage() async {
+    var storedEntries = storage.getItem('timeEntries');
+    if (storedEntries != null) {
+      _entries = List<TimeEntry>.from(
+        (storedEntries as List).map((item) => TimeEntry.fromJson(item)),
+      );
+      notifyListeners();
+    }
+  }
+
   void addTimeEntry(TimeEntry entry) {
     _entries.add(entry);
     notifyListeners();
